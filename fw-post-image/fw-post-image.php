0e172692b028896f13ba4e335c58848dcc93b6e8
fairweb <fairweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-07 21:46:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fw-post-image/tags/1.0@180684 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fw-post-image/fw-post-image.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package fw-post-image
+ * @author Myriam Faulkner
+ * @version 1.0
+ */
+/*
+Plugin Name: fw-post-image
+Plugin URI: http://weblog.fairweb.fr/plugins-wordpress/fw-post-image
+Description: If no post image is defined, get first image belonging to post or first image found post content. WP 2.9 > only.
+Author: Myriam Faulkner
+Version: 1.0
+Author URI: http://Fairweb.fr/
+*/
+
+/*  Copyright 2009  Myriam Faulkner  (email : web@fairweb.fr)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ */
+require_once(WP_PLUGIN_DIR .'/fw-post-image/fw-post-image.class.php');
+
+    $fw_pimg = new fw_post_image();
+    add_filter( 'post_image_html', array(&$fw_pimg, 'get_post_image'),1,5 );
+
+?>
